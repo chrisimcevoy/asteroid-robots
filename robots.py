ef1154c7e710fac6291fef4189e26abe29d082fb
james <jamesgodev1@gmail.com>
2021-10-24 02:05:03 +0100

    Initial commit - checking Gitlab integration.

---
+++ robots.py
@@ -0,0 +1,12 @@
+"""
+James Gough coding challenge.
+24/10/2021
+
+Robots module that interprets JSON from a text file in order to provide the final coordinates of the robot's position
+and bearing
+"""
+
+class Robot:
+
+    def __init__(self):
+        pass
